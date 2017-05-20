595da7384b4fbf0c1e0a47d8926b299e2bc00cab
Alvarocz <alvaromaster2015@gmail.com>
2017-05-20 14:11:41 -0500

    create Image model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Image(models.Model):
+    nombre = models.CharField(max_length=20)
+    url = models.CharField(max_length=150)
+    calificacion = models.IntegerField(validators=[MinValueValidator(1),
+                                                   MaxValueValidator(5)])
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Image
 
 def index(request):
     return render(request, 'index.html')
 
 def galeria(request):
-    return render(request, 'galeria.html')
+    images = Image.objects.order_by('-calificacion')
+    return render(request, 'galeria.html', context={'images': images})
