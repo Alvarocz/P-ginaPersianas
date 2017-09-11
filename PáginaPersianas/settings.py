1692137193897a9e63406af356b20467edb01ee7
Alvarocz <alvaromaster2015@gmail.com>
2017-09-11 16:21:01 -0500

    fix staticfiles error

--- PáginaPersianas/settings.py
+++ PáginaPersianas/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
     'home',
 ]
 
